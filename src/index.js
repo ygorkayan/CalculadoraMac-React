675bbdadfaa36ca38f9446958b7306959c6a9dc9
Ygor Kayan <ygorkayan@gmail.com>
2020-04-28 21:52:53 -0300

    inicio

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculadora from "./main/Calculator";
+
+import "./index.css";
+
+ReactDOM.render(
+    <div>
+        <h1>Calculadora</h1>
+        <Calculadora />
+    </div>,
+    document.getElementById("root")
+);
